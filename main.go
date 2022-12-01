e9dbe788aeda89aeb6ef5584b728264685d47350
Anibal Andrade <59257555+androso@users.noreply.github.com>
2022-12-01 18:04:58 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	// Read contents of input.txt
+	// [[500, 600, 700], [800, 900], [1000]]
+	// [1800, 1700, 1000]
+	// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? -> 1800
+		
+}
